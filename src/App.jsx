9e9acb5bdc9aff67b41bcdab670abf3ecb1f4e20
feliciano <rodinofeliciano@gmail.com>
2024-08-05 16:15:51 +0100

    actualizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import  {Form} from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <p>testando...</p>
-      </div>
+      <Form/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Form/index.jsx
@@ -0,0 +1,36 @@
+import styles from "./Form.module.css"
+
+export function Form() {
+    return(
+
+        <div className={styles.form}>
+            <form id="form" metoh="post" styles="background-color: #csc;">
+                <select>
+                    <option>...</option>
+                </select><br/><br/>
+                
+                <div class="input-single">
+                    <input type="text" id="titulo" class="input" autocomplete="off"/>
+                    <label for="nome">Titulo</label>
+                </div>
+                <br/><br/>
+
+                <div class="input-single">
+                    <input type="text" id="titulo" name="subtitulo" autocomplete="off"/>
+                    <label for="nome">Sub-titulo</label>
+                </div>
+                <br/><br/>
+
+                <div class="input-single">
+                    <textarea class="mensagem" id="mensagem" placeholder="Escreva aqui sua mensagem"/>
+                </div>
+                <br/><br/>
+
+                <div class="btn">
+                    <button type="submit" id="enviar" name="enviar">Enviar</button>
+                </div>
+            </form>
+        </div>
+
+    );    
+};
