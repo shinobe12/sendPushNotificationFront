a781823f46dd312e28ef8bdf4e80c89c4c0a280b
feliciano <rodinofeliciano@gmail.com>
2024-08-06 10:15:40 +0100

    actualizando

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -7,29 +7,32 @@ export function Form() {
             <form id="form" metoh="post" styles="background-color: #csc;">
                 <select class="select">
                     <option>...</option>
+                </select>
+                <select class="select-metodo">
+                    <option>...</option>
                 </select><br/><br/>
                 
                 <div class="input-single">
+                <label for="nome">Titulo</label>
                     <input type="text" id="titulo" class="input" autocomplete="off"/>
-                    <label for="nome">Titulo</label>
+                    
                 </div>
                 <br/><br/>
 
                 <div class="input-single">
-                    <input type="text" id="titulo" name="subtitulo" autocomplete="off"/>
                     <label for="nome">Sub-titulo</label>
+                    <input type="text" id="titulo" name="subtitulo" autocomplete="off"/>
+                    
                 </div>
                 <br/><br/>
 
                 <div class="input-single">
-                    <textarea class="mensagem" id="mensagem" placeholder="Escreva aqui sua mensagem"/>
+                    <textarea class="mensagem" id="mensagem" placeholder="  Escreva aqui sua mensagem"/>
                 </div>
-                <br/><br/>
-
-                <div class="btn">
-                    <button type="submit" id="enviar" name="enviar">Enviar</button>
-                </div>
+                
+                <button type="submit" class="enviar" id="enviar">Enviar</button>
             </form>
+            
         </div>
 
     );    
