b0c6f2857dc1b52c1b61cd5a8d2d8c5e4e4b2c3a
feliciano <rodinofeliciano@gmail.com>
2024-08-15 17:22:28 +0100

    actualizando pop ups

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -3,14 +3,14 @@
 export function Lista() {
     return (
       <main className='min-h-screen bg-zinc-950 dark:bg-[#fff]'>
-          <div className="flex justify-center grid grid-cols-3 mt-12">
+          <div className="flex justify-center mt-12 grid grid-cols-3">
             <div></div>
-            <div className='w-full md:w-full p-8 bg-[#272729] dark:bg-[#fff] rounded-lg text-white text-center mt-10'>
+            <div className=' p-8 bg-[#272729] dark:bg-[#fff] rounded-lg text-white text-center mt-10'>
               
               <div className=" p-4 rounded-lg ring-1 ring-[#D4D4D4]">
-                  <div className="grid grid-cols-2 mt-3">
-                    <div className="text-bold dark:text-zinc-900">Aplicativo: ...</div>
-                    <div className="text-bold dark:text-zinc-900">Data: ...</div>
+                  <div className="flex justify-between p-2 mt-3">
+                    <div className="text-bold dark:text-zinc-900">Aplicativo: </div>
+                    <div className="text-bold dark:text-zinc-900">Data: </div>
                   </div>
                   <div className="text-justify mt-3 bg-[#454545] dark:bg-[#E8F2FF] rounded-lg p-3">
                     <h2 className="text-start font-semibold dark:text-zinc-900">Titulo da Mensagem: ...</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{html,cjs,tsx,jsx,ts}"],
   theme: {
-    extend: {},
-    borderRadius:{
-      'lg': '30px 30px 30px 30px',
-      'md': '5px 5px 5px 5px',
-      'full': '100%'
-    },
-    keyframes: {
-      "fade-in": {
-          "0%": {
-              opacity: 0
-          },
-          "100%": {
-              opacity: 1
-          },
+    extend: {
+      borderRadius:{
+        'lg': '30px 30px 30px 30px',
+        'md': '5px 5px 5px 5px',
+        'full': '100%'
       },
-    }
+      
+      keyframes: {
+        "fade-in": {
+            "0%": {
+                opacity: 0
+            },
+            "100%": {
+                opacity: 1
+            },
+        },
+      },
+      animation: {
+        fade: 'fadeIn .5s ease-in-out',
+      },
+  
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
 
   },
   plugins: [],
